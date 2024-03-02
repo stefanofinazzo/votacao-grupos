18bb63d235ea26e173d3ba38f5e5859439229741
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-02 11:16:37 -0300

    Create 2_Administração.py

---
+++ pages/2_Administração.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#token de admin, se necessário implementar sistema de autenticação e autorização
+#para fins de jogos corporativos, não é necessário, e um token hardcoded
+#é suficiente
+TOKEN = 'admin'
+
+st.title('Página de Gerenciamento da Votação')
+
+#implementar inserção do token
