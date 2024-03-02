69fc0ae7b566abbcb74c5bb1e38d9c3b1536ae7d
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-02 11:08:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+n_grupos = 7
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#para testes, obviamente
+DICT_VOTANTES = {'ABCDEF': 1, 
+                  'BACBAC': 2,
+                 }
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if not em_votacao in st.session_state:
+    st.session_state['em_votacao'] = False
+    
+st.title('Sistema de votação')
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if not st.session_state['em_votacao']: 
+    votante_atual = st.text_input('Coloque seu token de votação')
+    
+    if votante_atual in lista_votantes:
+        st.session_state['em_votacao'] = False
+        st.session_state['grupo_votante'] = DICT_VOTANTES[votante_atual]
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+else:
+    #em votação
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    grupos_a_votar = list(range(1,8))
+    grupos_a_votar.remove(st.session_state['grupo_votante'])
+    
+    grupo_escolhido = st.selectbox('Escolha o seu grupo', grupos_a_votar)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    if grupo_escolhido:
+        st.write('Você escolheu o grupo ' + str(grupo_escolhido))
+    
