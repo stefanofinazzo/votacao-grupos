1cdd44d7264fcd26005d6b216002963c2ad447bd
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-03 17:55:05 -0300

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,6 +1,22 @@
+import string
+import secrets
+
+ALPHABET = string.ascii_letters
+
 from st_supabase_connection import SupabaseConnection
 
-def insert_votante(nome: str, email: ): 
-data, count = supabase.table('countries')
-  .insert({"id": 1, "name": "Denmark"})
-  .execute()
+def create_token(digits: int = 4):
+  
+  token = ''.join(secrets.choice(alphabet) for i in range(digits)).upper()
+   
+  return token
+  
+def insert_votante(nome: str, email: str): 
+
+  token = create_token()
+  
+  _, _ = supabase.table('votantes')
+    .insert({"nome": nome, 
+             "email": email,
+             "token": })
+    .execute()
