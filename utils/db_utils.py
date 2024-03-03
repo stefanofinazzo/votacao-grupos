73ffaa0ac46d9ea035ff437446b7e1249a8e76fd
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-03 17:57:05 -0300

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -10,7 +10,12 @@ def create_token(digits: int = 4):
   token = ''.join(secrets.choice(alphabet) for i in range(digits)).upper()
    
   return token
-  
+
+def connect_supabase():
+  conn = st.connection("supabase",type=SupabaseConnection)
+
+return conn
+
 def insert_votante(nome: str, email: str): 
 
   token = create_token()
