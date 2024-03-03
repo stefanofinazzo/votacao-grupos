c977ac2e9a2e92d336c4e30e1e638a542f197ede
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-03 17:49:52 -0300

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,1 +1,6 @@
+from st_supabase_connection import SupabaseConnection
 
+def insert_votante(nome: str, email: ): 
+data, count = supabase.table('countries')
+  .insert({"id": 1, "name": "Denmark"})
+  .execute()
