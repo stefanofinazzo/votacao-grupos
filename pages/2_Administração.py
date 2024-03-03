d574bcdc20153df3a8fd36dd179a0f7a607dd915
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-03 18:00:03 -0300

    Update 2_Administração.py

--- pages/2_Administração.py
+++ pages/2_Administração.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils import db_utils
 
 #token de admin, se necessário implementar sistema de autenticação e autorização
 #para fins de jogos corporativos, não é necessário, e um token hardcoded
@@ -7,4 +8,12 @@ TOKEN = 'admin'
 
 st.title('Página de Gerenciamento da Votação')
 
-#implementar inserção do token
+with st.form("my_form"):
+   st.write("Inside the form")
+   nome = st.text_input('Nome')
+   email = st.text_input('E-mail')
+   
+   submitted = st.form_submit_button("Cadastrar votante")
+   if submitted:
+       db_utils.
+
