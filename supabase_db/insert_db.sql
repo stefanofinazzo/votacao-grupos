276b7af9d101b16dd4dd916a1f173432d9b9419e
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-04 14:18:31 -0300

    Update insert_db.sql

--- supabase_db/insert_db.sql
+++ supabase_db/insert_db.sql
@@ -3,3 +3,5 @@ INSERT INTO votantes VALUES ('exemplo@gmail.com', 'Exemplo de Usuário',  1, 'AB
 INSERT INTO votos
 (voto)
 VALUES ('Voto sim!');
+
+INSERT INTO perguntas VALUES (1, 'O que você acha desta pergunta teste?');
