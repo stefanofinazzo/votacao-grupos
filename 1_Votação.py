cd0f3e12d01d5c6cb9e97f1a1ae439cdae3479f5
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-04 14:25:45 -0300

    Update 1_Votação.py

--- 1_Votação.py
+++ 1_Votação.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title='Sistema de Votação', 
                    page_icon=None, 
-                   layout="centered", 
+                   layout="wide", 
                    initial_sidebar_state="collapsed", 
                    menu_items=None)
 
