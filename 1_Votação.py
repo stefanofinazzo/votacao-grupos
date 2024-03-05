9984766acf73b55dd460bfb48004945fa26b1e8c
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-05 13:48:33 -0300

    Update 1_Votação.py

--- 1_Votação.py
+++ 1_Votação.py
@@ -45,10 +45,10 @@ def widget_verifica_votante(conn):
 def widget_em_votacao(conn, app_config: dict) -> None:
 
     pergunta_atual_id = app_config['pergunta_liberada']
-    pergunta_atual_nome = db_utils.localiza_pergunta(conn, pergunta_atual_id)
+    pergunta_atual_text = db_utils.localiza_pergunta(conn, pergunta_atual_id)['pergunta_texto']
   
     st.markdown('## Pergunta em votação: ' + str(pergunta_atual_id))
-    st.markdown('### ' + pergunta_atual_nome)
+    st.markdown('### ' + pergunta_atual_text)
     
     if st.button('Votar'):
         pass
