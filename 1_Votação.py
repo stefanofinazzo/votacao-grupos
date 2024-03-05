3efde2419c6d8736248f167003eecfaaff97a3e8
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-05 12:52:08 -0300

    Update 1_Votação.py

--- 1_Votação.py
+++ 1_Votação.py
@@ -1,51 +1,63 @@
+################ PACOTES ############################
+
 from time import sleep
 
 import streamlit as st
+
+from utils import db_utils
+
+########## CONFIGURAÇÃO DO STREAMLIT ################
 
 st.set_page_config(page_title='Sistema de Votação', 
                    page_icon=None, 
                    layout="wide", 
                    initial_sidebar_state="collapsed", 
                    menu_items=None)
-
-n_grupos = 7
-
-#para testes, obviamente
-#necessário lógica para obter os tokens de um db qualquer aqui
-#para os fins do aplicativo (para jogos corporativos simples, não
-#é necessário implementar ferramentas de autenticação/autorização/etc.
-DICT_VOTANTES = {'ABCDEF': 1, 
-                  'BACBAC': 2,
-                 }
-
-if not 'em_votacao' in st.session_state:
-    st.session_state['em_votacao'] = False
     
-st.title('Sistema de votação')
+################### WIDGETS #########################
 
-if not st.session_state['em_votacao']: 
-    votante_atual = st.text_input('Coloque seu token de votação')
+def mainpage():
+    st.title('Sistema de votação')
     
-    if votante_atual in list(DICT_VOTANTES.keys()):
-        st.session_state['em_votacao'] = True
-        st.session_state['grupo_votante'] = DICT_VOTANTES[votante_atual]
-        st.success('Bem-vindo!')
-        sleep(2)
-        st.rerun()
+    conn = connect_supabase()
+    app_config = get_config(conn)
 
-    elif votante_atual is not None:
-        st.error('Votante não localizado')
+    if not 'votante_autorizado' in st.session_state:
+        st.session_state['votante_autorizado'] = False
+      
+    if not st.session_state['votante_autorizado']: 
 
-else:
-    #em votação
+        votante_atual = st.text_input('Coloque seu token de votação')
+        
+        if votante_atual in list(DICT_VOTANTES.keys()):
+            st.session_state['votante_autorizado']
+            st.session_state['grupo_votante'] = DICT_VOTANTES[votante_atual]
+            st.success('Bem-vindo!')
+            sleep(2)
+            st.rerun()
+    
+        elif votante_atual is not None:
+            st.error('Votante não localizado')
+    
+    else:
+        #em votação
+        pergunta_atual = app_config['pergunta_liberada']
+        
+        st.markdown('## Pergunta em votação: ' + pergunta_atual)
+        st.markdown('### ' + 'PERGUNTA ATUAL AQUI')
+      
+        grupos_a_votar = list(range(1,8))
+        grupos_a_votar.remove(st.session_state['grupo_votante'])
+        
+        grupo_escolhido = st.selectbox('Escolha o seu grupo', grupos_a_votar)
+        
+        if st.button('Votar'):
+            st.write('Você escolheu o grupo ' + str(grupo_escolhido))
+            #lógica para transferir o voto para db aqui
+            #lógica para remover voto da lista aqui
 
-    grupos_a_votar = list(range(1,8))
-    grupos_a_votar.remove(st.session_state['grupo_votante'])
-    
-    grupo_escolhido = st.selectbox('Escolha o seu grupo', grupos_a_votar)
-    
-    if st.button('Votar'):
-        st.write('Você escolheu o grupo ' + str(grupo_escolhido))
-        #lógica para transferir o voto para db aqui
-        #lógica para remover voto da lista aqui
-    
+################### PÁGINA PRINCIPAL ###################
+
+if __name__ = '__main__':
+    mainpage()
+
