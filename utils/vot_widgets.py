d9aa542a14a7db39d562dccb56c93ad635e19e1e
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-12 12:28:16 -0300

    Update vot_widgets.py

--- utils/vot_widgets.py
+++ utils/vot_widgets.py
@@ -23,6 +23,14 @@ def lista_perguntas_no_banco(perguntas_df: pd.DataFrame):
 def votos_bar_plot(votos_pergunta_df: pd.DataFrame):
 
       fig = px.bar(votos_pergunta_df, y='voto', x='n_votos')
+      fig.update_layout(
+                yaxis = dict(
+                    tickmode = 'linear',
+                    tick0 = 1,
+                    dtick = 1
+                )
+            )
+      
       fig.show()
 
       st.plotly_chart(fig, use_container_width=True)
