1bf8aa9e9adbec6b70d9d886efba3dd4ab9b8449
stefanofinazzo <53883063+stefanofinazzo@users.noreply.github.com>
2024-03-14 12:17:07 -0300

    Update st_tools.py

--- utils/st_tools.py
+++ utils/st_tools.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 def remove_bar():
   
-  hide_decoration_bar_style = """
-      <style>
-          header {visibility: hidden;}
-      </style>
-      """
-  
-  st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+    hide_decoration_bar_style = """
+        <style>
+            header {visibility: hidden;}
+        </style>
+        """
+    
+    st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
